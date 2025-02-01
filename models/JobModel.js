b314ac80155c33c8ca584cde16e84854e1c56201
paulmcgonigle <S00210326@atu.ie>
2025-02-01 12:30:32 -0600

    added mongoDB model functions and also StatusCodes

--- models/JobModel.js
+++ models/JobModel.js
@@ -1,21 +1,25 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const JobSchema = new mongoose.Schema({
-    company:String, 
-    postion:String, 
-    jobStatus:{
-        type:String,
-        enum:['interview', 'declined', 'pending'] ,default:'pending'   },
-    jobType:{
-        type:String,
-        enum:['full-time', 'part-time', 'internship'] ,default:'full-time'   },
-    jobLocation:{
-        type:String,
-        default:'my-city'
-    }
-    
+const JobSchema = new mongoose.Schema(
+  {
+    company: String,
+    position: String,
+    jobStatus: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    jobType: {
+      type: String,
+      enum: ['full-time', 'part-time', 'internship'],
+      default: 'full-time',
+    },
+    jobLocation: {
+      type: String,
+      default: 'my city',
+    },
+  },
+  { timestamps: true }
+);
 
-
-},{
-    timestamps:true
-})
+export default mongoose.model('Job', JobSchema);
